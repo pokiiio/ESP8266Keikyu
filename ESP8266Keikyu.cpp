c3f50f637b61620fb8be7f9627badbfdbc6548b1
pokio <32353873+pokiiio@users.noreply.github.com>
2017-12-03 16:59:51 +0900

    Add files via upload

---
+++ ESP8266Keikyu.cpp
@@ -0,0 +1,80 @@
+#include <ESP8266HTTPClient.h>
+#include "ESP8266Keikyu.h"
+
+int ESP8266Keikyu::getStatus()
+{
+  return getUnkoStatus(getUnkoInfo());
+}
+
+String ESP8266Keikyu::getUnkoInfo()
+{
+  HTTPClient http;
+
+  // ページをすべてGETしようとすると落ちるので、ページの先頭からではなく途中からGETする
+  // ページの構成が変わると死ぬ
+  int offset = 20000;
+
+  // 運行情報はこのコメントに囲まれている
+  // コメントが変わると死ぬ
+  String unkoPanelComment = "<!-- ======================== 運行情報 =================================== -->";
+
+  // 京急の運行ページをパースします
+  http.begin("http://unkou.keikyu.co.jp/");
+
+  // Rangeの設定をして、部分的にGETする
+  http.addHeader("Range", "bytes=" + String(offset) + "-" + String(offset + 10000));
+
+  int httpCode = http.GET();
+
+  String result = "";
+
+  if (httpCode > 0 && http.getSize() > 0)
+  {
+    result = http.getString();
+  }
+
+  http.end();
+
+  if (result.indexOf(unkoPanelComment) > 0)
+  {
+    result = result.substring(result.indexOf(unkoPanelComment) + unkoPanelComment.length());
+    result = result.substring(0, result.indexOf(unkoPanelComment));
+  }
+
+  return result;
+}
+
+int ESP8266Keikyu::getUnkoStatus(String unkoInfo)
+{
+  // 含まれている文字列で運行状況を判断
+  // 今後運用が変わったら死ぬ可能性がある
+
+  if (unkoInfo.indexOf("受託") > 0)
+  {
+    return KEIKYU_STATUS_DELAYED_DUE_TO_JR;
+  }
+
+  if (unkoInfo.indexOf("見合わせ") > 0)
+  {
+    return KEIKYU_STATUS_STOPPED;
+  }
+
+  if (unkoInfo.indexOf("乱れ") > 0)
+  {
+    return KEIKYU_STATUS_TERRIBLE;
+  }
+
+  if (unkoInfo.indexOf("遅れ") > 0 || unkoInfo.indexOf("運休") > 0)
+  {
+    return KEIKYU_STATUS_DELAYED;
+  }
+
+  if (unkoInfo.indexOf("平常") > 0)
+  {
+    return KEIKYU_STATUS_NORMAL;
+  }
+
+  return KEIKYU_STATUS_UNKNOWN;
+}
+
+ESP8266Keikyu Keikyu;
---
+++ ESP8266Keikyu.h
@@ -0,0 +1,29 @@
+#ifndef ESP8266KEIKYU_H
+#define ESP8266KEIKYU_H
+
+#if (ARDUINO >= 100)
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class ESP8266Keikyu
+{
+public:
+  int getStatus(); // 下で定義している定数を返します
+
+private:
+  String getUnkoInfo();               // 京急のページから運行情報の文字列を取得します
+  int getUnkoStatus(String unkoInfo); // 運行情報の文字列からステータスを判断します
+};
+
+extern ESP8266Keikyu Keikyu;
+
+#define KEIKYU_STATUS_UNKNOWN 0           // 京急の運行情報が取得できません
+#define KEIKYU_STATUS_NORMAL 1            // 京急は平常通り運転してます
+#define KEIKYU_STATUS_DELAYED 2           // 遅れています
+#define KEIKYU_STATUS_TERRIBLE 3          // ダイヤが乱れています
+#define KEIKYU_STATUS_STOPPED 4           // 運転を見合わせています
+#define KEIKYU_STATUS_DELAYED_DUE_TO_JR 5 // JRからの振替輸送受託のため遅れています
+
+#endif
